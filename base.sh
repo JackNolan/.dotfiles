7d65c7222c72c387c35d2e836bed861a2a2d89e2
Jack Nolan <jack@jacknolan.com>
2012-11-07 15:40:09 -0500

    the world has burned

--- base.sh
+++ base.sh
@@ -1,27 +1,4 @@
-export JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Home"
-source ~/.dotfiles/git.sh
-source ~/.dotfiles/bin_path.sh
-source ~/.dotfiles/sm.sh
-source ~/.dotfiles/ruby.sh
-source ~/.dotfiles/rails.sh
-source ~/.dotfiles/editor.sh
-source ~/.dotfiles/homebrew.sh
-source ~/.dotfiles/heroku.sh
-source ~/.dotfiles/opscode.sh
-source ~/.dotfiles/mysql.sh
-source ~/.dotfiles/misc.sh
-source ~/.dotfiles/node.sh
-source ~/.dotfiles/php.sh
-source ~/.dotfiles/engineyard.sh
-source ~/.dotfiles/cloud_foundry.sh
-source ~/.dotfiles/functions.sh
+source  ~/.dotfiles/git.sh
 source ~/.dotfiles/bash_shortcuts.sh
-source ~/.dotfiles/rails_completion.sh
-export github_username="drnic"
-# source ~/.dotfiles/seinfeld.sh/seinfeld.sh
-
-[[ -s ~/.dotfiles/development_setups.sh ]] && source ~/.dotfiles/development_setups.sh
 source ~/.dotfiles/bashprompt.sh
-
-
 # Last
