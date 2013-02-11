b019733c7e3edd5a0de354add63f587802ce5567
Jack Nolan <jack.nolan@jack-nolan.local>
2013-02-11 14:13:56 -0600

    disabled rails completion

--- base.sh
+++ base.sh
@@ -3,5 +3,5 @@ source ~/.dotfiles/bash_shortcuts.sh
 source ~/.dotfiles/bashprompt.sh
 source ~/.dotfiles/bash_completion.sh
 source ~/.dotfiles/rake_completion.sh
-source ~/.dotfiles/rails_completion.sh
+# source ~/.dotfiles/rails_completion.sh
 # Last
